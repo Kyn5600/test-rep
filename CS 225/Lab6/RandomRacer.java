c0bd9254a6820455b2a4fbf74efd564e1283f188
ironraptor03 <ironraptor03@gmail.com>
2023-02-26 17:34:22 -0500

    lab6 commitblock

--- CS 225/Lab6/RandomRacer.java
+++ CS 225/Lab6/RandomRacer.java
@@ -1,3 +1,23 @@
+/*************************************
+ * Class RandomRacer
+ * Author: 
+ * 
+ * Purpose: Racer class with random speed.
+ * 
+ * Attributes:  location: int
+ *              racerNumber: int
+ * 				racerType: String
+ * 
+ * Methods: +move():void
+ * 			#randomFrom(int,int):int
+ * 			+getLocation():int
+ * 			+setLocation(int):void
+ * 			+getRacerNumber():int
+ * 			+setRacerNumber(int):void
+ * 			+getRacerType():String
+ * 			+setRacerType(String):void
+ * 			#printRacer():void
+ *************************************/
 public class RandomRacer extends GenericRacer{
 
     public RandomRacer(int id) {
