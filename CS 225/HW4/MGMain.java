abb3e3cf496ba91e26517804a4a4c82b0a304ace
Kyn5600 <sniperzach075@gmail.com>
2023-03-03 14:20:51 -0500

    HW4 Complete

--- CS 225/HW4/MGMain.java
+++ CS 225/HW4/MGMain.java
@@ -1,7 +1,9 @@
 //Edits: Removed both createMaze methods. Reason: Redundant for GameController createMaze methods.
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
@@ -9,7 +11,7 @@ import java.util.Random;
 public class MGMain {
     private GameController ctrl;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException{
         MGMain ctrl = new MGMain();
         Random rnd = new Random();
         rnd.setSeed(rnd.nextLong());
@@ -22,12 +24,28 @@ public class MGMain {
         if(mazeOrRand.equals("file")){
             // Assume file is CSV or text file set up like this:
             // x,y,True,True,False,False
-            // or x,y,l,u,u,l
-            //System.out.println("Enter Filename: ");
-            //String filename = scan.nextLine();
-            //FileReader fr = new FileReader(filename);
-            //BufferedReader br = new BufferedReader(fr);
-            //...
+            
+            System.out.println("Enter Filename: ");
+            String filename = scan.nextLine();
+            File file = new File(filename);
+            int length = (int)file.length();
+            FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(fr);
+            
+            gc.createMaze(length, length);
+
+            String row;
+            while ((row = br.readLine() ) != null) {
+                String[] cols = row.split(",");
+                int x = Integer.parseInt(cols[0]);
+                int y = Integer.parseInt(cols[1]);
+                ArrayList<Boolean> fileBool = new ArrayList<Boolean>();
+                for(int i = 3;i<7;i++)
+                    fileBool.add(Boolean.parseBoolean(cols[i]));
+                gc.createMaze(x, y,fileBool);
+            }
+            br.close();
+            fr.close();
         } else if(mazeOrRand.equals("random")){
             ArrayList<Boolean> isLockedTrue = new ArrayList<Boolean>();
             System.out.println("What size would you like the maze to be? Please enter an odd number.");
